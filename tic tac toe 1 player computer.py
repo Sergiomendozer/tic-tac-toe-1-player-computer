28ef1a3d425fd2664c369cd6f65c0b8a1dbf343d
sergiomendozer <sergiomendozansx@gmail.com>
2022-01-08 14:45:11 -0800

    pops the list for bot, next add did_x_win

--- tic tac toe 1 player computer.py
+++ tic tac toe 1 player computer.py
@@ -4,29 +4,33 @@ Green="\033[0;32m"        # Green
 Blue="\033[0;34m"         # Blue
 Purple="\033[0;35m"       # Purple
 
-def player_x(player, Bot, tic_tac_toe_chart,):
+def player_x(player, Bot, tic_tac_toe_chart,list_of_positions_for_bot_to_select):
     print(tic_tac_toe_chart) 
     if player == "X":
         position = input ("select a position you would like to place X:")
-        replace = tic_tac_toe_chart.find(position)
+        delete = list_of_positions_for_bot_to_select.index(position)
+        list_of_positions_for_bot_to_select.pop(delete)
+        print (list_of_positions_for_bot_to_select) ### to delete
         replace = tic_tac_toe_chart.find(position)
         tic_tac_toe_chart = tic_tac_toe_chart[:replace] + 'X' + tic_tac_toe_chart[replace+1:]
-        print(tic_tac_toe_chart) 
+        print(tic_tac_toe_chart) #### delete?
     elif Bot == "X":
         print ("will do next")
 
 
 def x_or_o (player):
     tic_tac_toe_chart = "1 2 3 \n4 5 6\n7 8 9\n"
-    list_of_positions_to_select = [1,2,3,4,5,6,7,8,9]
+    list_of_positions_for_bot_to_select = ["1","2","3","4","5","6","7","8","9"]
     if player == "x" or player == "X":
         player = "X"
         Bot = "O"
         print("You chose X")
+        return player_x(player, Bot, tic_tac_toe_chart,list_of_positions_for_bot_to_select)
     elif player == "o" or player == "O":
         player = "O"
         Bot = "X"
         print("You chose O")
+        return player_x(player, Bot, tic_tac_toe_chart,list_of_positions_for_bot_to_select)
     else:
         player = str(input(Red +"You must choose X or O:"+ END)) 
         return x_or_o (player)
